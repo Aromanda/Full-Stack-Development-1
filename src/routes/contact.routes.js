13735792f9ce23bc0d0899a8e417dc63f672a2ad
ET GLap <eglapointe@hotmail.com>
2023-06-20 13:07:01 -0400

    fix: delete unnecessary files (doublon)

--- src/routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-const ContactController = require('../features/contact/contact.controller');
-
-
-const registerContactRoutes = (app) => {
-    app.post('/contact', ContactController.createContact);
-}
-
-module.exports = {registerContactRoutes};
