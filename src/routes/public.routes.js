ad12aa6987587974ed8b494ea74020a5fc76b77c
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-06-23 09:23:58 -0400

    add middleware in the routes

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -1,7 +1,9 @@
 const PublicController = require('../features/public/public.controller');
+const validateContactdata = require('../shared/middleware/base-middleware');
+const decMiddleWare = require('../shared/middleware/base-middleware');
 
 const registerPublicRoutes = (app) => {
-  app.post('/contact', PublicController.contactUs);
+  app.post('/contact', validateContactdata.validateContactdata, decMiddleWare.decMiddleWare, PublicController.contactUs);
 
   app.get('/calc/:buildingType', PublicController.calculateQuote);
 }
