013096b552919e61a02396c5360d7550a9cb45e4
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-06-23 09:24:28 -0400

    create middleware in the base middleware

--- src/shared/middleware/base-middleware.js
+++ src/shared/middleware/base-middleware.js
@@ -1,6 +1,28 @@
 require('dotenv').config();
 const Express = require('express');
 const app = Express();
+const validator = require('validator');
+
+const decMiddleWare = (req, res, next) => {
+  console.log('Running middleware for:', req.path);
+  next();
+}
+
+const validateContactdata = (req, res, next) => {
+  const { fullname, email, phone, message } = req.body;
+
+  if (!fullname || !phone || !email || !message) {
+    return res.status(400).json({ error: "All fields are required"})
+  }
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ error: "Invalid email address" });
+  }
+  if (!validator.isMobilePhone(phone)) {
+    return res.status(400).json({ error: "Invalid phone number" });
+  }
+  next();
+}
+
 
 const adminRoutes = [
   '/email-list',
@@ -14,7 +36,7 @@ const registerBaseMiddleWare = (app) => {
   app.use(checkAuthToken);
 };
 
-const logger = (req,res,next) => {
+/*const logger = (req,res,next) => {
   const message = `API call: ${req.method} on ${req.originalUrl} at ${new Date()}`
   console.log(message);
   next();
@@ -37,6 +59,6 @@ const checkAuthToken = (req,res,next) => {
     return;
   }
   next();
-};
+};*/
 
-module.exports = {registerBaseMiddleWare};
+module.exports = {validateContactdata, decMiddleWare};
