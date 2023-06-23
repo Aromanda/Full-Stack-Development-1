1280522c5e3c5f7eb67fa1535abb60c27c7e07e5
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-06-23 14:28:30 -0400

    modify structure routes

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -1,11 +1,10 @@
 const PublicController = require('../features/public/public.controller');
-const validateContactdata = require('../shared/middleware/base-middleware');
-const decMiddleWare = require('../shared/middleware/base-middleware');
+const { validateContactdata, decMiddleWare, validateBuildingType } = require('../shared/middleware/base-middleware');
 
 const registerPublicRoutes = (app) => {
-  app.post('/contact', validateContactdata.validateContactdata, decMiddleWare.decMiddleWare, PublicController.contactUs);
+  app.post('/contact', validateContactdata, decMiddleWare, PublicController.contactUs);
 
-  app.get('/calc/:buildingType', PublicController.calculateQuote);
+  app.get('/calc/:buildingType', validateBuildingType, decMiddleWare, PublicController.calculateQuote);
 }
 
 module.exports = {registerPublicRoutes};
