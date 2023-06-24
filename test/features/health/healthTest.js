aca40c3d830a2f34b7e9c85c53f646f3309e56c4
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-06-24 11:35:00 -0400

    add my test for status and error

--- test/features/health/healthTest.js
+++ test/features/health/healthTest.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const sinon = require('sinon');
-const HealthController = require('../src/features/health/health.controller');
-const ResponseUtil = require('../src/shared/utils/response-util').ResponseUtil;
+const HealthController = require('../../../src/features/health/health.controller');
+const ResponseUtil = require('../../../src/shared/utils/response-util').ResponseUtil;
 
 describe('HealthController',()=>{
   afterEach(() => {
@@ -14,8 +14,30 @@ describe('HealthController',()=>{
         chai.assert.equal(message,'Hello World');
         done();
       });
-      
       void HealthController.helloWorld();
+    });
+  });
+
+  describe('#status()',()=>{
+    const envName = process.env.ENV_NAME;
+    const port = 3004;
+    it(`respond with Environment '${envName}' running on port: '${port}`,(done)=>{
+      sinon.stub(ResponseUtil,'respondOk').callsFake((res,data,message)=>{
+        chai.assert.equal(message,`Environment '${envName}' running on port: ${port}`);
+        done();
+      });
+      void HealthController.status();
+    });
+  });
+
+  describe('#error()',()=>{
+    it('respond with error and status code 400',(done)=>{
+      sinon.stub(ResponseUtil,'respondOk').callsFake((res,data,message)=>{
+        chai.assert.equal(message, `error`);
+        //chai.assert.equal(status, 400);
+        done();
+      });
+      void HealthController.error();
     });
   });
 });
